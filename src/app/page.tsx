3e213f92ffcde2cb15080e28d3f0f7e3a83812af
fahad-islam <fahadislam.dev@gmail.com>
2023-05-01 10:44:11 +0500

    fixed grid for large size view point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="w-full h-max min-h-screen px-8 2xl:px-14  py-8">
-      <div className="flex flex-col gap-8 w-full min-h-full">
-        <div className="">{`Dashboard > Purchased Applications`}</div>
+    <main className=" w-full h-max min-h-screen px-7 2xl:px-14  py-8 flex justify-center">
+      <div className="max-w-screen-2xl flex flex-col gap-8 w-full min-h-full">
+        <div className="min-w-max">{`Dashboard > Purchased Applications`}</div>
         <ApplicationCardTiles />
       </div>
     </main>
